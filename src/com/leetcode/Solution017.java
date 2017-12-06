9071180d86c47e618672f9a7b5485b8d5e4d9b1e
qiaolaoye <392011109@qq.com>
2017-12-06 11:42:04 +0800

    first commit

--- src/com/leetcode/Solution017.java
+++ src/com/leetcode/Solution017.java
@@ -1,9 +1,24 @@
 package com.leetcode;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Solution017 {
     public static List<String> letterCombinations(String digits) {
+        LinkedList<String> ans = new LinkedList<>();
+        String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
+        if (digits.length() == 0) return ans;
+        ans.add("");
+
+        for (int i = 0; i < digits.length(); i++) {
+            int x = Character.getNumericValue(digits.charAt(i));
+            while (ans.peek().length() == i) {
+                String t = ans.remove();
+                for (Character s : mapping[x].toCharArray())
+                    ans.add(t + s);
+            }
+        }
+        return ans;
     }
 }
