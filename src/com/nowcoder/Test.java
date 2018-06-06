8a0636837a9d3bd29e9684b64a7cf9b1e349c048
qiaolaoye <392011109@qq.com>
2018-06-06 22:36:13 +0800

    update huawei code interviews.

--- src/com/nowcoder/Test.java
+++ src/com/nowcoder/Test.java
@@ -1,8 +1,12 @@
 package com.nowcoder;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println(new String("aa 0sjB9kK s").toUpperCase());
+        System.out.println((char)('C' + 33));
     }
 
 }
